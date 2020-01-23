353fd980fadc764d4bd47a5c4873501aba2392e5
Marta Fondevila <55930814+MartaFonde@users.noreply.github.com>
2020-01-23 22:34:10 +0100

    Update VentasMain.java

--- Ventas/VentasMain.java
+++ Ventas/VentasMain.java
@@ -6,19 +6,21 @@ public class VentasMain {
         int ano;
         int opcion;
         do {
-            System.out.println("Introduce año");
-            ano = sc.nextInt();
-            sc.nextLine();
-            Ventas ventas1 = new Ventas(ano);
-            ventas1.grafica();
-            System.out.printf("Media: %.2f\n\n", ventas1.media());
-
-            Ventas ventas2 = new Ventas(2000);
-            ventas2.grafica();
-            System.out.printf("Media: %.2f\n\n", ventas2.media());
+            if(args.length == 0){
+                System.out.println("Introduce año");
+                ano = sc.nextInt();
+                sc.nextLine();
+                Ventas ventas1 = new Ventas(ano);
+                ventas1.grafica();
+                System.out.printf("Media: %.2f\n\n", ventas1.media());
+            } else {
+                int anoComando=Integer.parseInt(args[0]);
+                Ventas ventas2 = new Ventas(anoComando);
+                ventas2.grafica();
+                System.out.printf("Media: %.2f\n\n", ventas2.media());
+            }    
             System.out.println("¿Repetir?\n 1. Sí\n 2. No");
             opcion = sc.nextInt();
         } while (opcion == 1);
-
     }
 }
